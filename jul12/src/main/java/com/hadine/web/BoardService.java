ef3d96dcff6ff4c1120efa1cd56de4be81a775f7
hjsong96 <ghkwlsdbwls@gmail.com>
2023-07-12 17:29:11 +0900

    jul12

---
+++ jul12/src/main/java/com/hadine/web/BoardService.java
@@ -0,0 +1,14 @@
+package com.hadine.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+	
+	@Autowired
+	private BoardDAO boardDAO;
+	//Controller -> Service -> DAO -> sqlSession -> DB
+	
+	
+}
